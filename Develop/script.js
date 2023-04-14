db4a7775fe9b877fe7a869b6fc0b12e17aa8f52f
Mohammad Al-Abedredha <malabedredha@gmail.com>
2023-04-14 00:04:35 -0500

    added a save button

--- Develop/script.js
+++ Develop/script.js
@@ -8,7 +8,15 @@ $(function () {
   // function? How can DOM traversal be used to get the "hour-x" id of the
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
-  //
+
+  const saveBtn = document.querySelectorAll(".save");
+  saveBtn.forEach((saveBtn) => {
+    saveBtn.addEventListener("click", function () {
+      const hour = this.closest(".data-time").id
+      localStorage.setItem(hour, description)
+    })
+  })
+
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
   // attribute of each time-block be used to conditionally add or remove the
