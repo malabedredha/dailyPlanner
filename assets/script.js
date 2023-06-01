60bc3068d50ea6864915d76cbd565b73085a67cf
Mohammad Al-Abedredha <malabedredha@gmail.com>
2023-05-31 21:20:00 -0500

    script.js

--- assets/script.js
+++ assets/script.js
@@ -1,23 +1,39 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-$(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
+let currentDay = document.querySelector("#currentDay");
+let containerPX5 = document.querySelector(".px-5");
+let buttonArray = [];
+let newText = document.querySelectorAll(".description");
+
+// dayjs format
+currentDay.textContent = dayjs().format("dddd," + "MMM DD, YYYY" + " h:mm A");
+
+containerPX5.addEventListener("click", function (event) {
+// Checks for button elements
+  if (event.target.matches("i") || event.target.matches("button")) {
+    // Loop
+    for (let i = 0; i < newText.length; i++) {
+      // uses local storage to store text inside textboxes
+      localStorage.setItem(newText[i].parentNode.textContent, newText[i].value);
+    }
+  }
 });
+
+// Decides what the color should be based off past present and future
+for (let i = 0; i < 9; i++) {
+  
+  let timeEl = document.querySelector("#hour-" + (i + 9));
+  let presentTime = dayjs().hour();
+
+  if (
+    parseInt(document.querySelectorAll(".row")[i].dataset.time) < presentTime
+  ) {
+    timeEl.classList.add("past");
+
+  } else if (
+    parseInt(document.querySelectorAll(".row")[i].dataset.time) === presentTime
+  ) {
+    timeEl.classList.add("present");
+
+  } else {
+    timeEl.classList.add("future");
+  }
+}
